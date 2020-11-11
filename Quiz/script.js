60633cae535e617244e2777b6973e74e9846a4de
Andrea Rus <andrearus27@gmail.com>
2020-11-11 21:24:50 +0000

    Update script.js

--- Quiz/script.js
+++ Quiz/script.js
@@ -7,11 +7,14 @@ form.addEventListener('submit', e => {
   let score = 0;
   const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
-  userAnswers.forEach((answer, index)) {
+  userAnswers.forEach((answer, index) => {
     if(answer === correctAnswers[index]) {
       score += 25;
-    }
   }
-})
+});
+
+console.log(score);
+
+});
 
 
